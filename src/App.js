a732c697d2db98c2d9a2cc87aa3d973d2e2ac47c
shuvamk <kshuvam564@gmail.com>
2020-10-27 01:16:44 -0700

    Added the SpotLight

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const Lights=()=>{
     <ambientLight intensity={0.3} />
     <directionalLight position={[10,10,5]} intensity={1}/>
     <directionalLight position={[0,10,0]} intensity={1.5}/>
+    <spotLight intensity={1} position={[1000,0,0]} />
     </>
   )
 }
