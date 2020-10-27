21190bd2a4f7049fcc6294db63c76020a1b12dfb
shuvamk <kshuvam564@gmail.com>
2020-10-27 01:49:36 -0700

    Added the Scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useEffect, useRef } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber';
 import './App.scss';
 import Header from './components/header';
 import {Html, RoundedBox, useGLTFLoader} from 'drei'
 import { Section } from './components/section';
+import state from './components/state';
+import { useInView } from 'react-intersection-observer';
+
 
 const Model=({modelPath})=>{
   const gltf= useGLTFLoader(modelPath, true)
@@ -19,11 +22,17 @@ const Lights=()=>{
     </>
   )
 }
-const HtmlContent=({children, modelPath, positionY})=>{
+const HtmlContent=({children,domContent, modelPath, positionY, bgColor})=>{
   const ref= useRef();
+  const [refItem, inView]= useInView({
+    threshold:0
+  })
   useFrame(()=>{
     ref.current.rotation.y+=0.01;
   })
+  useEffect(()=>{
+    inView && (document.body.style.backgroundColor= bgColor)
+  },[inView])
   return( 
     <Section factor={1.5} offset={1}>
       <group position={[0,positionY,0]}>
@@ -33,15 +42,25 @@ const HtmlContent=({children, modelPath, positionY})=>{
           <Model modelPath={modelPath}/>
         </mesh>
       <Html
+      portal={domContent}
         fullscreen 
         >
-          {children}
+          <div ref={refItem}>
+            {children}
+          </div>
+
         </Html>
       </group>
     </Section>
   )
 }
 function App() {
+  const domContent= useRef();
+  const scrollArea= useRef();
+  const onScroll=(e)=>{
+    state.top.current= e.target.scrollTop
+  }
+  useEffect(()=>void onScroll({target: scrollArea.current}),[])
   return (
     <>
       <Header />
@@ -52,6 +71,8 @@ function App() {
         <Lights />
         <Suspense fallback={null}>
            <HtmlContent 
+           bgColor={'#f15946'}
+           domContent={domContent}
            modelPath='/armchairYellow.gltf'
            positionY={250}>
           <div className='container'>
@@ -59,14 +80,29 @@ function App() {
           </div>
            </HtmlContent>
            <HtmlContent 
+           bgColor={'#571ec1'}
+           domContent={domContent}
            modelPath='/armchairGreen.gltf'
            positionY={0}>
           <div className='container'>
             <h1 className='title'>Hello</h1>
           </div>
            </HtmlContent>
+           <HtmlContent 
+           bgColor={'#636567'}
+           domContent={domContent}
+           modelPath='/armchairGray.gltf'
+           positionY={-250}>
+          <div className='container'>
+            <h1 className='title'>Hello</h1>
+          </div>
+           </HtmlContent>
         </Suspense>
       </Canvas>
+      <div className='scrollArea' ref={scrollArea} onScroll={onScroll}>
+        <div style={{position:'sticky', top:0}} ref={domContent}></div>
+        <div style={{height: `${state.sections*100}vh`}}></div>
+      </div>
     </>
   );
 }
